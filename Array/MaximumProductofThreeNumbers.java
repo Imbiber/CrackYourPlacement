ba7f6512d4d92d60d21f85251b84d3b0a6151265
Nishant Gaurav <ng19nishant@gmail.com>
2024-07-15 13:40:52 +0530

    Find maximum product of Three Numbers in an Array

---
+++ Array/MaximumProductofThreeNumbers.java
@@ -0,0 +1,15 @@
+package Array;
+
+import java.util.Arrays;
+
+public class MaximumProductofThreeNumbers {
+    
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n=nums.length;
+        int max1= nums[n-3]*nums[n-2]*nums[n-1];
+        int max2=nums[0]*nums[1]*nums[n-1];
+        return Math.max(max1,max2);
+        
+    }
+}
